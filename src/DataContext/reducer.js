fa2275a5cbf814e97e3b897ef8c58a205a5f4d7b
Vatsal <vatsalpatel1073@gmail.com>
2021-05-06 13:48:05 +0530

    reducer and state provider added

---
+++ src/DataContext/reducer.js
@@ -0,0 +1,27 @@
+// inital state of the app.
+
+export const initialState = {
+  user: null,
+  cart: null,
+};
+
+// helper function : will eventually get the cart total.
+
+export const cartTotal = (cart) =>
+  cart?.reduce((amount, item) => item.price + amount, 0);
+
+//reducer function : used to manipulate the state : add or grab data from the context
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+  }
+};
+
+export default reducer;
