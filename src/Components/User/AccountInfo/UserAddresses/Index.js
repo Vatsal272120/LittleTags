cc433948ae26b11c8573c005c32abf03ee51e9f5
Vatsal <vatsalpatel1073@gmail.com>
2021-05-11 11:05:32 +0530

    working on user Address page

--- src/Components/User/AccountInfo/UserAddresses/Index.js
+++ src/Components/User/AccountInfo/UserAddresses/Index.js
@@ -1,11 +1,57 @@
 import React from "react";
-import { MainWrapper, Container, PageHeader } from "./Styles";
+import {
+  MainWrapper,
+  Container,
+  PageHeader,
+  HeaderDetails,
+  HeaderButton,
+  AddressDetails,
+  UserAddress,
+  Segment,
+  SegmentContent,
+} from "./Styles";
+import { Link, useHistory } from "react-router-dom";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { useStateValue } from "../../../../DataContext/StateProvider";
 
 const Address = () => {
+  const [{ user }, dispatch] = useStateValue();
+  const history = useHistory();
+
   return (
     <MainWrapper>
       <Container>
-        <PageHeader></PageHeader>
+        <PageHeader>
+          <Link className='link' to='/account'>
+            <ArrowBackIosIcon fontSize='small' />
+            Back to Account
+          </Link>
+          <HeaderDetails>
+            <h1 className='headerDetails__heading'>My addresses</h1>
+            <HeaderButton>Add a new address</HeaderButton>
+          </HeaderDetails>
+        </PageHeader>
+
+        <AddressDetails>
+          <UserAddress>
+            <Segment>
+              <h2 className='segmentTitle'>Default Address</h2>
+              <SegmentContent>
+                <p className='accountAddress'>
+                  <span className='one'>{user?.displayName}</span>
+                  <br />
+                  {user?.addressLineOne}
+                  <br />
+                  {user?.addressLineTwo}
+                  <br />
+                  {user?.city}
+                  <br />
+                  {user?.state}
+                </p>
+              </SegmentContent>
+            </Segment>
+          </UserAddress>
+        </AddressDetails>
       </Container>
     </MainWrapper>
   );
--- src/Components/User/AccountInfo/UserAddresses/Styles.js
+++ src/Components/User/AccountInfo/UserAddresses/Styles.js
@@ -18,7 +18,173 @@ export const PageHeader = styled.div`
   position: relative;
   display: block;
 
+  .link {
+    display: inline-block;
+    margin-bottom: 25px;
+    font-size: 12px;
+    color: #939393;
+    transition: color 0.2s ease-in-out;
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+    font-family: Futura, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    background-color: transparent;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .MuiSvgIcon-root {
+    height: 9px;
+    margin-right: 12px;
+    vertical-align: initial;
+    overflow: hidden;
+    width: 1em;
+    fill: currentColor;
+    display: inline-block;
+    stroke-width: 1px;
+    background: none;
+    pointer-events: none;
+    font-size: 12px;
+    color: #939393;
+    font-family: Futura, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+    cursor: pointer;
+  }
+
   @media screen and (min-width: 641px) {
     margin: 50px 0;
+  }
+`;
+
+export const HeaderDetails = styled.div`
+  display: block;
+
+  .headerDetails__heading {
+    margin-top: 0 !important;
+    font-size: 22px;
+    font-family: Futura, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    color: #5c5c5c;
+    transition: color 0.2s ease-in-out;
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    line-height: 1.65;
+  }
+
+  @media screen and (min-width: 1008px) {
+    margin-bottom: 70px;
+  }
+`;
+
+export const HeaderButton = styled.button`
+  display: block;
+  color: white;
+  border-color: #ff9999;
+  position: relative;
+  display: inline-block;
+  padding: 14px 28px;
+  line-height: normal;
+  border: 1px solid transparent;
+  border-radius: 0;
+  background-color: #ff9999;
+  text-transform: uppercase;
+  font-size: 11px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  font-family: Futura, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  transition: color 0.45s cubic-bezier(0.785, 0.135, 0.15, 0.86),
+    border 0.45s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+  z-index: 1;
+  -webkit-tap-highlight-color: initial;
+  -webkit-appearance: button;
+  cursor: pointer;
+  overflow: visible;
+  margin: 0;
+  -webkit-writing-mode: horizontal-tb !important;
+  text-rendering: auto;
+  word-spacing: normal;
+  text-indent: 0px;
+  text-shadow: none;
+
+  @media screen and (min-width: 1008px) {
+    margin-top: 30px;
+  }
+`;
+
+export const AddressDetails = styled.div`
+  margin-bottom: -60px;
+  display: block;
+  list-style: none;
+  padding: 0;
+  margin: 0 0 0 -24px;
+  font-size: 0;
+
+  @media screen and (min-width: 1240px) {
+    margin-left: -80px;
+  }
+`;
+
+export const UserAddress = styled.div`
+  margin-bottom: 60px;
+  display: inline-block;
+  padding: 0 0 0 24px;
+  margin: 0;
+  vertical-align: top;
+  font-size: 1rem;
+
+  @media screen and (min-width: 1240px) {
+    padding-left: 80px;
+  }
+
+  @media screen and (min-width: 1008px) {
+    width: 33.33333%;
+  }
+`;
+
+export const Segment = styled.div`
+  .segmentTitle {
+    margin-bottom: 34px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #e7e7e7;
+    color: #939393;
+    font-size: 12px;
+    font-family: Futura, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    transition: color 0.2s ease-in-out;
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+    margin-top: 0;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+`;
+
+export const SegmentContent = styled.div`
+  .accountAddress {
+    margin-top: 0;
+    font-size: inherit;
+    font-weight: inherit;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-style: normal;
+    color: #5c5c5c;
+    font-family: "Century Gothic", sans-serif;
   }
 `;
