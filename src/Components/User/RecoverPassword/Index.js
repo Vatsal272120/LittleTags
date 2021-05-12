7b786069d57ceac46eaf67095755fe52ff67618c
Vatsal <vatsalpatel1073@gmail.com>
2021-05-12 17:27:49 +0530

    users can recover the password now

--- src/Components/User/RecoverPassword/Index.js
+++ src/Components/User/RecoverPassword/Index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useHistory } from "react-router-dom";
 import {
@@ -14,9 +14,29 @@ import {
   LoginSocials,
 } from "./../Login/Styles";
 
+import { auth } from "../../../Utils/firebaseUtility";
+
 const Recover = () => {
   const history = useHistory();
+
+  const [email, setEmail] = useState("");
+
   const redirectToLoginPage = () => history.push("/account");
+
+  const recoverPassword = (e) => {
+    e.preventDefault();
+
+    auth
+      .sendPasswordResetEmail(email)
+      .then((response) => {
+        console.log(response);
+        auth.signOut();
+        history.push("/account/login");
+      })
+      .catch((e) => alert(e.message));
+
+    console.log("recovering password Now");
+  };
 
   return (
     <LoginWrapper>
@@ -36,13 +56,17 @@ const Recover = () => {
                   type='email'
                   className='formInput'
                   placeholder='Email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   autofocus
                   required
                 />
                 <label className='floatingLabel'>Email</label>
               </LoginFormItem>
 
-              <LoginSubmit>Recover</LoginSubmit>
+              <LoginSubmit type='submit' onClick={recoverPassword}>
+                Recover
+              </LoginSubmit>
               <LoginHint>
                 <span onClick={redirectToLoginPage} className='restore'>
                   Back to Login Page
